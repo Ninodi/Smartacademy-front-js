d6397addf07e58d6c312b2a11afc90b2c33fd64d
Ninodi <78281735+Ninodi@users.noreply.github.com>
2021-03-12 02:04:51 +0400

    hw 12

---
+++ main.js
@@ -0,0 +1,23 @@
+// task 1
+
+var a = 13
+
+var b = 15
+
+var result = a + b
+
+if (result % 2 === 0){
+    console.log("ლუწი");
+}else if(result % 2 === 1){
+    console.log("კენტი");
+}
+
+// task 2
+
+b = 10
+
+h = 6
+
+S = 1/2 * b * h
+
+console.log(S)
